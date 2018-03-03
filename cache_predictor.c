8742b6b2912f2c334909b244d95b64d58c93fa28
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-03-03 13:27:15 -0500

    Add mode

--- cache_predictor.c
+++ cache_predictor.c
@@ -18,7 +18,6 @@
 // Number of trials to access the memory
 #define TRIALS 1000000
 
-
 // Create storage
 int arr[MAX_SIZE];
 
@@ -40,7 +39,7 @@ void generate_cache_performance(int number_of_pages) {
 
   start = clock();
 
-  // TODO: Do the experiment here
+  // Do the experiment here
   int i = 0;
   for(; i < TRIALS; i++) {
     int j = 0;
@@ -56,7 +55,67 @@ void generate_cache_performance(int number_of_pages) {
   printf("%u, %lf\n", number_of_pages, total_time / n);
 }
 
-int main() {
+double get_element_access_time(int index) {
+  struct timespec start_t, end_t;
+  double start, end, delta;
+
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_t);
+  arr[index] += 1;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_t);
+
+  start = 1000000000 * start_t.tv_sec + start_t.tv_nsec;
+  end = 1000000000 * end_t.tv_sec + end_t.tv_nsec;
+  delta = end - start;
+
+  printf("%d, %lf\n", index, delta);
+
+  return delta;
+}
+
+void run_cache_size_test() {
+  // Initiate array
+  fill_array(10485760);
+
+  printf("Number of trials: %d\n", TRIALS);
+  printf("Generating cache performance data...\n");
+
+  int i = 2;
+  while (i <= 4096) {
+    generate_cache_performance(i);
+    i = i * 2;
+  }
+} 
+
+void run_cache_line_test() {
+  int i  = 2;
+  while (i <= 10000) {
+    get_element_access_time(i);
+
+    if (i == 1024 || i == 2048 || i == 4096 || i == 8192) {
+      int j = 1;
+      while (j <= 10) {
+        get_element_access_time(i + j);
+        j++;
+      } 
+    }
+    i = i * 2;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int mode;
+  char *p;
+  if (argc < 2) {
+    printf("Usage: ./cache_predictor operation_number\n" \
+        "1: Generate cache size data\n" \
+        "2: Generate cache line data\n");
+    return 0;
+  }
+  else {
+    long conv = strtol(argv[1], &p, 10);
+    mode = conv;
+  }
+
   // For multi processor architecture, we want this process to be run in a processor only
   cpu_set_t set;
   CPU_ZERO(&set);
@@ -68,18 +127,22 @@ int main() {
   if (sched_setaffinity(getpid(), sizeof(set), &set) == -1) {
     exit(1);
   }
-  
-  // Initiate array
-  fill_array(10485760);
 
-  printf("Number of trials: %d\n", TRIALS);
-  printf("Generating cache performance data...\n");
-  
-  int i = 2;
-  while (i <= 4096) {
-    generate_cache_performance(i);
-    i = i * 2;
+  switch(mode) {
+    case 1:
+      // Cache size test
+      printf("------------------------------------\n");
+      run_cache_size_test();
+      break;
+    case 2:
+      // Cache line test
+      printf("------------------------------------\n");
+      run_cache_line_test();
+      break;
   }
+
+
+  
 
   return 0;
 }
